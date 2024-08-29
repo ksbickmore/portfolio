dd8c9ef6aae715a9980d8b673426466468bfbf2d
Kristopher Bickmore <12804658+ksbickmore@users.noreply.github.com>
2024-08-29 13:15:38 +0000

    more updates

---
+++ src/Components/Education.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// import image from '../images/education.jpg';
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>Education</h2>
+        <p>
+          I have a Bachelor&rsquo;s degree in Computer Science from the University of Maryland.
+          Currently I&rsquo;m pursuing a Master&rsquo;s degree in Software Engineering from the
+          University of Maryland.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,17 +9,6 @@ import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/cyber.jpg";
 
 const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
@@ -33,7 +22,11 @@ const Home = ({ name, title }) => {
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img
+          src={arrowSvg}
+          style={{ height: "3rem", width: "3rem", animation: "bounce 1s infinite" }}
+          alt={imageAltText}
+        />
       </div>
     </section>
   );
